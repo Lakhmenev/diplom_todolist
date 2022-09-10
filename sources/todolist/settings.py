8c2d20469542ee8bd482b2c874e2677448d52448
dmitrii_lakhmenev <lahmenev@gmail.com>
2022-09-10 17:28:26 +0300

    static update3

--- sources/todolist/settings.py
+++ sources/todolist/settings.py
@@ -122,17 +122,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'core.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('deploy', 'nginx', 'static')
+STATIC_ROOT = str(BASE_DIR.parent.joinpath('deploy', 'nginx', 'static'))
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'core.User'
