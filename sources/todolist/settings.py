418e463c2a50bc2f5ad597957f43d68aa1897208
dmitrii_lakhmenev <lahmenev@gmail.com>
2022-09-10 16:46:05 +0300

    static update2

--- sources/todolist/settings.py
+++ sources/todolist/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.parent.joinpath('deploy', 'nginx', 'static')
+STATIC_ROOT = BASE_DIR.joinpath('deploy', 'nginx', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
