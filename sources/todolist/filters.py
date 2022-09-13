d8fed821cbc65a8bea59150eede1f3eef20eabcc
Dmitriy Lahmenev <lahmenev@gmail.com>
2022-09-13 16:07:21 +0300

    edit filter

--- sources/todolist/filters.py
+++ sources/todolist/filters.py
@@ -4,4 +4,4 @@ import logging
 class HealthCheckFilter(logging.Filter):
 
     def filter(self, record) -> bool:
-        return record.getMessage().find('/ping/') == -1
+        return record.getMessage().find('/ping') == -1
