92b4dd6023e50bfb80cc10bd8e833c51c7f995dc
Dmitriy Lahmenev <lahmenev@gmail.com>
2022-10-17 16:35:44 +0300

    bot

--- sources/bot/views.py
+++ sources/bot/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics, permissions
-
 from bot.models import TgUser
 from bot.serializers import TgUserSerializer
+from bot.tg.client import TgClient
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from todolist import settings
 
 
 class VerificationView(generics.GenericAPIView):
@@ -9,10 +11,14 @@ class VerificationView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = TgUserSerializer
 
-    def dispatch(self, request, *args, **kwargs):
+    def patch(self, request, *args, **kwargs):
         s: TgUserSerializer = self.get_serializer(data=request.data)
         s.is_valid(raise_exception=True)
 
         tg_user: TgUser = s.validated_data['tg_user']
         tg_user.user = self.request.user
-        tg_user.save( )
+        tg_user.save(update_fields=('user', ))
+
+        instance_s: TgUserSerializer = self.get_serializer(tg_user)
+        TgClient(settings.BOT_TOKEN).send_message(tg_user.chat_id, '[verification_has_been_completed]')
+        return Response(instance_s.data)
